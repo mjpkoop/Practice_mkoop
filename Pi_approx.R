c4a05cd71d9e256a9c26501095f446042fcadeb6
mjpkoop <mjpkoop@gmail.com>
2019-08-17 14:24:55 -0400

    Create Pi_approx.R

---
+++ Pi_approx.R
@@ -0,0 +1,8 @@
+# Approx pi
+pi_approx = function(darts = 500) {
+  dx = runif(darts, -1,1)
+  dy = runif(darts, -1,1)
+  in_circle = which(dx**2 + dy**2 < 1)
+  pi = 4 * (length(in_circle)/darts)
+  return(pi)
+}
